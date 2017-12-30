3aa5394b3a5681f9139efbee6f499d66bfabb803
Scott Baker <sbaker1505@gmail.com>
2017-12-30 13:56:15 -0800

    Tab Nav added to App.js

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { TabNavigator, StackNavigator } from 'react-navigation'
+import { MaterialCommunityIcons, Ionicons } from '@expo/vector-icons'
 
 import DeckList from './components/DeckList'
 import AddCard from './components/AddCard'
@@ -7,15 +9,64 @@ import Deck from './components/Deck'
 import NewDeck from './components/NewDeck'
 import Quiz from './components/DeckList'
 
+import { white, blue } from './utils/colors'
+
+
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <DeckList />
+      <View style={{flex: 1}}>
+        <View style={{height: 30}}/>
+        <Tabs />
       </View>
     );
   }
 }
+
+const Tabs = TabNavigator({
+  DeckList: {
+    screen: DeckList,
+    navigationOptions: {
+      tabBarLabel: 'Decks',
+      tabBarIcon: ({ tintColor }) =>
+        <MaterialCommunityIcons
+          name='cards'
+          size={30}
+          color={tintColor}
+        />
+    }
+  },
+  NewDeck: {
+    screen: NewDeck,
+    navigationOptions: {
+      tabBarLabel: 'New Deck',
+      tabBarIcon: ({ tintColor }) =>
+        <Ionicons
+          name='ios-add-circle'
+          size={30}
+          color={tintColor}
+        />
+    }
+  }
+},{
+  navigationOptions: {
+    header: null
+  },
+  tabBarOptions: {
+    activeTintColor: blue,
+    style: {
+      height: 56,
+      backgroundColor: white,
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+})
 
 const styles = StyleSheet.create({
   container: {
