173a7645e8a435cb962c0866bd3e4ca7657b96bb
Scott Baker <sbaker1505@gmail.com>
2017-12-31 15:41:27 -0800

    Added KeyboardAvoidingView to NewDeck

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-import { View, TouchableOpacity, Text, StyleSheet, TextInput } from 'react-native'
+import { View, TouchableOpacity, Text, StyleSheet, TextInput, KeyboardAvoidingView } from 'react-native'
 
-import { white, blue, gray } from '../utils/colors'
+import { white, blue, gray, red } from '../utils/colors'
 
 import TextButton       from './TextButton'
 
@@ -11,23 +11,25 @@ class NewDeck extends Component {
   }
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.text}>What is the title of your new deck?</Text>
-        <TextInput
-          placeholder='Deck Title'
-          autoFocus={true}
-          maxLength={30}
-          placeholderTextColor={gray}
-          value={this.state.title}
-          style={styles.input}
-          onChangeText={(text) => this.setState({title: text})}
-        />
-        <TextButton
-          style={styles.button}
-          onPress={() => console.log('Pressed')}>
-          Submit
-        </TextButton>
-      </View>
+      <KeyboardAvoidingView behavior='padding' style={styles.keyboardContainer}>
+        <View style={styles.container}>
+          <Text style={styles.text}>What is the title of your new deck?</Text>
+          <TextInput
+            placeholder='Deck Title'
+            autoFocus={true}
+            maxLength={30}
+            placeholderTextColor={gray}
+            value={this.state.title}
+            style={styles.input}
+            onChangeText={(text) => this.setState({title: text})}
+          />
+          <TextButton
+            style={styles.button}
+            onPress={() => console.log('Pressed')}>
+            Submit
+          </TextButton>
+        </View>
+      </KeyboardAvoidingView>
     )
   }
 }
@@ -35,6 +37,9 @@ class NewDeck extends Component {
 export default NewDeck
 
 const styles = StyleSheet.create({
+  keyboardContainer: {
+    flex: 1
+  },
   container: {
     flex: 1,
     margin: 10,
@@ -52,7 +57,8 @@ const styles = StyleSheet.create({
     width: '80%',
     borderColor: blue,
     borderBottomWidth: 1,
-    fontSize: 16
+    fontSize: 18,
+    color: red
   },
   button: {
     marginTop: 10,
@@ -61,6 +67,6 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 30,
     color: blue,
-    textAlign: 'center'
+    textAlign: 'center',
   }
 })
